9759a816435cb53f88a05eb77f6343eebe88fe74
Mohammed Talha <sulemanshah432@gmail.com>
2022-10-15 02:45:43 +0000

    constructor injection

---
+++ constructor-injec/src/main/java/com/spring/app/Customer.java
@@ -0,0 +1,44 @@
+package com.spring.app;
+
+public class Customer {
+    private int custId;
+    private String custName;
+    private String emailId;
+    private long contactNo;
+    
+    public Customer(int custId, String custName, String emailId, long contactNo){
+        super();
+        this.custId = custId;
+        this.custName = custName;
+        this.emailId = emailId;
+        this.contactNo = contactNo;
+    }
+
+    public void setCustId(int custId){
+        this.custId = custId;
+    }
+    public int getCustId(){
+        return custId;
+    }
+
+    public void setCustName(String custName){
+        this.custName = custName;
+    }
+    public String getCustName(){
+        return custName;
+    }
+
+    public void setEmailId(String emailId){
+        this.emailId = emailId;
+    }
+    public String getEmailId(){
+        return emailId;
+    }
+
+    public void setContactNo(long contactNo){
+        this.contactNo = contactNo;
+    }
+    public long getContactNo(){
+        return contactNo;
+    }
+}
